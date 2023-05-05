7c8a555dd21a5e258124f79c2d6ce41ee8c1cc4d
Eliane Henriqueta <elianehenriqueta@gmail.com>
2023-05-05 13:49:27 -0300

    feat: Criar preview na listagem

--- src/components/meet/MeetObjectsRoom.tsx
+++ src/components/meet/MeetObjectsRoom.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @typescript-eslint/no-extra-non-null-assertion */
 
 import thrashIcon from '../../assets/images/trash_object.svg';
